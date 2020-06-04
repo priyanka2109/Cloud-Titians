5ae1df68fd5bcfb6a4086eb130087f6ff7eeea18
priyanka2109 <57538743+priyanka2109@users.noreply.github.com>
2020-06-04 20:29:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_nav import Nav
+from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
+app = Flask(__name__, static_folder='static')
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("About.html")
+
+@app.route("/Contact")
+def contact():
+    return render_template("Contact.html")
+
+
+if __name__ == "__main__":
+    app.run()
